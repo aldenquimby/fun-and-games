947fabb58df927fab8f378118fbfe67641c8b865
Alden Quimby <aldenquimby@gmail.com>
2013-09-02 16:53:46 -0400

    triplets, another search problem

--- HackerRank/Challenges/Search/Triplets/Program.cs
+++ HackerRank/Challenges/Search/Triplets/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Triplets
 {
+    class Triplet
+    {
+        public int One { get; set; }
+        public int Two { get; set; }
+        public int Three { get; set; }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static string GetLine()
         {
+            return Console.ReadLine();
+        }
+
+        static void Main()
+        {
+            GetLine(); // don't need n
+            var d = GetLine().Split(' ').Select(int.Parse).ToList();
+            Console.WriteLine(Triplets(d));
+        }
+
+        static long Triplets(IList<int> d)
+        {
+            var triplets = new Triplet[d.Count];
+            var seen = new HashSet<int>();
+            for (var i = d.Count - 1; i >= 0; i--)
+            {
+                triplets[i] = new Triplet();
+                if (!seen.Contains(d[i]))
+                {
+                    triplets[i].One = 1;
+                    seen.Add(d[i]);
+                }
+            }
+
+            // TODO n^2 is too slow, timing out on half of the cases
+            for (var i = d.Count - 1; i >= 0; i--)
+            {
+                for (var j = i + 1; j < d.Count; j++)
+                {
+                    if (d[i] == d[j])
+                    {
+                        break;
+                    }
+                    
+                    if (d[i] < d[j])
+                    {
+                        triplets[i].Two += triplets[j].One;
+                        triplets[i].Three += triplets[j].Two;
+                    }
+                }
+            }
+
+            return triplets.Sum(x => (long) x.Three);
         }
     }
 }
